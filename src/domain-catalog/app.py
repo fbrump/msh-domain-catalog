473b4edc511ee8cef575e6c44905a40d083682e9
Brum <fbrump@gmail.com>
2017-12-18 23:56:59 -0200

    Created app.py with default value and config.py settings configurations. Config root and instance config.

--- src/domain-catalog/app.py
+++ src/domain-catalog/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 api = Api(app)
+
+app.config.from_object('config')
+app.config.from_pyfile('config.py')
 
 class HelloWorld(Resource):
     def get(self):
---
+++ src/domain-catalog/config.py
@@ -0,0 +1,5 @@
+print('config main')
+
+DEBUG = True # Turns on debugging features in Flask
+BCRYPT_LOG_ROUNDS = 12 # Configuration for the Flask-Bcrypt extension
+MAIL_FROM_EMAIL = "brum@mail.com" # For use in application emails
