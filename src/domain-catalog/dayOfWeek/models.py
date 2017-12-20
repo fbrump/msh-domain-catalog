49ec3b298b1722bf8edbba15ae7f4a5d34c7a083
Brum <fbrump@gmail.com>
2017-12-19 22:25:03 -0200

    Adicionado as configurações das colunas

--- src/domain-catalog/dayOfWeek/models.py
+++ src/domain-catalog/dayOfWeek/models.py
@@ -1,8 +1,18 @@
+from your_orm import Model, Column, Integer, String, DateTime
 
 class DayOfWeek(object):
 	"""docstring for DayOfWeek"""
+	code = Column(String)
+	abbr = Column(String)
+	description = Column(String)
+	date_created = Column(DateTime, auto_now_add=True)
 	def __init__(self, code, abbr, description):
 		super(DayOfWeek, self).__init__()
 		self.code = code
 		self.abbr = abbr
 		self.description = description
+
+class User(Model):
+	email = Column(String)
+	password = Column(String)
+	date_created = Column(DateTime, auto_now_add=True)
