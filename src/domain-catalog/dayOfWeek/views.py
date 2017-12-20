05c986265f665c3f19cfcb0a3de949d576459726
Brum <fbrump@gmail.com>
2017-12-19 23:06:53 -0200

    Created functionality for working with days of week, fixed on the code.

--- src/domain-catalog/dayOfWeek/views.py
+++ src/domain-catalog/dayOfWeek/views.py
@@ -1,29 +1,35 @@
+from flask import jsonify
 from flask_restful import Resource, fields, marshal_with
 from .models import DayOfWeek
+import uuid
 
-resource_fields = {
-    'code': fields.String,
-    'abbr': fields.String,
-    'description': fields.String,
-}
-
-def getListData():
-	_list = list()
-		
-	_list.append(DayOfWeek(1, 'MON', 'Monday'))
-	_list.append(DayOfWeek(1, 'MON', 'Monday'))
-	_list.append(DayOfWeek(1, 'MON', 'Monday'))
-	_list.append(DayOfWeek(1, 'MON', 'Monday'))
-	_list.append(DayOfWeek(1, 'MON', 'Monday'))
-	_list.append(DayOfWeek(1, 'MON', 'Monday'))
-	_list.append(DayOfWeek(1, 'MON', 'Monday'))
-
-	return _list
+LIST_DAYS_OF_WEEKS = [
+	{ 'code': 1, 'name':'Sunday'},
+	{ 'code': 2, 'name':'Monday'},
+	{ 'code': 3, 'name':'Tuesday'},
+	{ 'code': 4, 'name':'Wednesday'},
+	{ 'code': 5, 'name':'Thursday'},
+	{ 'code': 6, 'name':'Friday'},
+	{ 'code': 7, 'name':'Saturday'},
+]
 
 class DaysOfWeekView(Resource):
 	def get(self, **kwargs):
 		return { 
-			'Return':  None, # getListData()
-			'Protocol': '',
+			'Return':  LIST_DAYS_OF_WEEKS,
+			'Protocol': str(uuid.uuid4()),
 			'Message': 'Success'
 		}
+
+class DaysOfWeekItemView(Resource):
+	def get(self, id, **kwargs):
+		return { 
+			'Return':  self.getDay(id),
+			'Protocol': str(uuid.uuid4()),
+			'Message': 'Success'
+		}
+	def getDay(self, id):
+		for day in LIST_DAYS_OF_WEEKS:
+			if day['code'] == id:
+				return day
+		return None
