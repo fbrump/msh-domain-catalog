870ac8b18b2b1471b5591a06ff50833d2149438c
Brum <fbrump@gmail.com>
2017-12-22 00:13:05 -0200

    Added model for Type Contact Mail

--- src/domain-catalog/typeContactMail/models.py
+++ src/domain-catalog/typeContactMail/models.py
@@ -1,1 +1,21 @@
 # domain-catalog/typeContactMail/models.py
+
+from flask_restful import fields, marshal_with
+
+resource_fields = {
+	'code': fields.String,
+	'name': fields.String
+}
+
+class TypeContactMailDao(object):
+	def __init__(self, code, name):
+		self.code = code
+		self.name = name
+
+		# This field will not be sent in the response
+		self.status = 'active'
+
+class TypeContactMail(Resource):
+	@marshal_with(resource_fields)
+	def get(self, **kwargs):
+		return TypeContactMailDao(code=1, name='Personal')
