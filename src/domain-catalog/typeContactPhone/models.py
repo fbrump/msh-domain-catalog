c6bad398071e04720ef355d016cef03bb20c444b
Brum <fbrump@gmail.com>
2017-12-22 23:38:48 -0200

    Create all properties of Type Contact Phone

--- src/domain-catalog/typeContactPhone/models.py
+++ src/domain-catalog/typeContactPhone/models.py
@@ -1,1 +1,17 @@
 # domain-catalog/typeContactPhone/models.py
+
+class TypeContactPhone(object):
+	"""
+		This class mapping Type Contact Phone model with your properties.
+
+		Field:
+			Code 		- Code identify model
+			Description - Description about type
+			Active 		- Defined if this object is activate or not
+
+	"""
+	def __init__(self, code, description, active):
+		super(TypeContactPhone, self).__init__()
+		self.code = code
+		self.active = active
+		
